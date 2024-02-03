a3094c96fab7a2bea6a6242484f9ee22c1167c45
Matheus Hack <matheushack90@gmail.com>
2024-02-03 00:46:04 -0300

    conversion

--- src/Services/ConversionService.php
+++ src/Services/ConversionService.php
@@ -6,6 +6,9 @@ use MatheusHack\RdStation\Factories\ConversionFactory;
 use MatheusHack\RdStation\Requests\ConversionRequest;
 use MatheusHack\RdStation\Responses\GenericResponse;
 
+/**
+ *
+ */
 class ConversionService extends BaseService
 {
     /**
@@ -13,10 +16,14 @@ class ConversionService extends BaseService
      */
     protected $base = 'platform';
 
+    /**
+     * @param ConversionRequest $request
+     * @return GenericResponse
+     */
     public function create(ConversionRequest $request)
     {
         try {
-            $endpoint = $this->endpointByKey($this->base, '/conversions');
+            $endpoint = $this->endpointByKey($this->base, '/events?event_type=conversion');
             return $this->execute($endpoint, 'POST', (new ConversionFactory)->make($request));
         } catch (\Exception $e) {
             return (new GenericResponse)
